ca8bc1d06494823bd2143941c8a9d372a1afd066
Lucus Voiles <lucusvoiles@gmail.com>
2024-10-17 10:45:04 -0700

    question-1

--- Adventure.java
+++ Adventure.java
@@ -5,14 +5,24 @@ public class Adventure {
         Scanner choiceInput = new Scanner(System.in);
         // Questions
         boolean q1a = false;
-        boolean q2a = false;
+        boolean q1b = false;
+        boolean answered1 = false;
 
-        System.out.println(
-                "You have been brought by the Military to defuse a bomb. You do not know exacly where it is but there are two options. \n Option a: A-site or Option b: B-site. Where do you choose to go?");
-        String question1Answer = choiceInput.nextLine();
-
-        if (question1Answer.equals("a")) {
-            q1a = true;
+        while (!answered1) {
+            System.out.println(
+                    "You have been brought by the Military to defuse a bomb. You do not know exacly where it is but there are two options. \n Option a: A-site or Option b: B-site. Where do you choose to go?");
+            String question1Answer = choiceInput.nextLine();
+            if (question1Answer.equals("a")) {
+                q1a = true;
+                answered1 = true;
+            } else if (question1Answer.equals("b")) {
+                q1b = true;
+                answered1 = true;
+            } else {
+                System.out.println("please enter valid answer");
+                answered1 = false;
+            }
         }
+
     }
 }
